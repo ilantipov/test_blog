bf5a94ad16423a986c2874634d8dc035007105c7
ilantipov@gmail.com <ilantipov@gmail.com>
2020-10-27 08:26:48 +0300

    save

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,3 +1,5 @@
+@extends('layouts.app')
+@section('content')
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
@@ -41,3 +43,4 @@
         </form>
     </x-jet-authentication-card>
 </x-guest-layout>
+@endsection
